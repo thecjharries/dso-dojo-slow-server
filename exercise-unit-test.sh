ef8d65597b41bfe66951dd41afa647506059706e
CJ Harries <cj@wizardsoftheweb.pro>
2023-01-01 14:09:44 -0600

    Guard for tweaking the base exercise

--- exercise-unit-test.sh
+++ exercise-unit-test.sh
@@ -13,6 +13,11 @@ exercise_duration=$((SECONDS - start))
 echo "It took $exercise_duration seconds to complete the base request"
 docker-compose --file exercise-stack.yaml down
 
+if [ $exercise_duration -lt 5 ]; then
+    echo "Please leave the base exercise API_WAIT_SECONDS above 5 seconds"
+    exit 1
+fi
+
 docker-compose --file solution-stack.yaml up --detach
 sleep 10
 curl --silent http://10.0.0.3:8000/ping | jq --exit-status '.message == "pong"' >/dev/null || exit 1
